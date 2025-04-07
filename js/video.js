d0cf59226cb0c4b88463fae494b6bc83a13bf7fb
mmhatch <mmhatch@umich.edu>
2025-04-06 21:09:06 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -21,46 +21,49 @@ document.querySelector("#pause").addEventListener("click", function() {
 
 });
 document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Speed up video")
 	let change = video.playbackRate/10;
 	video.playbackRate = video.playbackRate - change;
-	console.log("slower "+video.playbackRate)
+	console.log("Speed is "+video.playbackRate)
 });
 document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Slow down video")
 	let change = video.playbackRate/9;
 	video.playbackRate = video.playbackRate + change;
-	console.log("faster "+video.playbackRate)
+	console.log("Speed is  "+video.playbackRate)
 });
 document.querySelector("#skip").addEventListener("click", function() {
 	let time = video.currentTime += 10;
 	if (time < video.duration){
-	video.currentTime += 10;
-	console.log("where "+video.currentTime);
+	// video.currentTime += 10;
+	console.log("Video current time is "+video.currentTime);
 	}
 	else{
 	video.currentTime = 0;
-	console.log("where "+video.currentTime);
+	console.log("Video current time is "+video.currentTime);
 	}
 });
 document.querySelector("#mute").addEventListener("click", function() {
 	if (video.volume == 0){
 	video.volume = 1;
 	document.getElementById("mute").innerHTML="Mute";
-	console.log("volume"+video.volume);
+	console.log("Unmute");
 	}
 	else{
 	video.volume = 0;
 	document.getElementById("mute").innerHTML="Unmute";
-	console.log("volume"+video.volume);
+	console.log("Mute");
 	}
 });
 document.querySelector("#slider").addEventListener("input", function() {
 	var slider = document.getElementById("slider").value
 	video.volume = slider/100;
-	console.log("volume"+video.volume);
+	console.log("The current value is "+video.volume);
 	document.getElementById("volume").innerHTML=slider+"%";
 });
 document.querySelector("#vintage").addEventListener("click", function() {
-	video.style.filter = "grayscale(100%)";
+	video.classList.add("oldSchool");
+	
 });
 document.querySelector("#orig").addEventListener("click", function() {
 	video.style.filter = "grayscale(0%)";
